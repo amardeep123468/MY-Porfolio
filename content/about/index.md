c83ec17fc62c8682fcd96b11b0aef5c82f3197c8
amardeep123468 <amardeepchauhanknp@gmail.com>
2025-08-23 19:37:07 +0530

    Initial commit

---
+++ content/about/index.md
@@ -0,0 +1,23 @@
+---
+title: 'About Me'
+avatar: 'amardeep.jpg'
+skills:
+  - C || C++ 
+  - Data Structures
+  - Algorithms
+  - HTML || CSS
+  - Bootstrap
+  - Javascript || Jquery
+  - ReactJS || NodeJS || ExpressJS
+  - Git || Github
+  - MongoBD || Heroku
+  - Firebase
+---
+
+Hello! Interestingly, I have been a student of Maths in my high school and got introduced to programming in my freshman year at [Lovely Professional University](https://www.lpu.in/). My interest in coding started back in 2020 during first year of B.tech . Since then, I started learning programming languages & solving problems based on data structure and algorithms.
+
+Today, I'm currently pursuing my Bachelors in Computer Science and Engineering (2020-2024) with [8.2 GPA aggregate](https://drive.google.com/file/d/1brJbT4YbCdQwL8zRaGmFkkFc2UAJUwH2/view?usp=share_link).
+
+Also I've knowledge of Web Development and created some projects on that using HTML, CSS, Javascript / React.js / Node.js, MongoDB and some other cool libraries and frameworks. I'm also part of some student organisations and Leading one of them. 
+
+Here is a list of the technologies that I've worked with.
---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'Amardeep Singh Chauhan',
+  siteDescription:
+    'Amardeep is a Web Developer, who loves learning new things and helping tech beginners.',
+  siteKeywords:
+    'Roopshree Udaiwal, Roopshree6902, Mern Stack Developer, web developer, javascript, girlscript, jodhpur, GDSC, GFG',
+  siteUrl: 'https://roopshreeudaiwal.netlify.app/',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Amardeep Singh Chauhan',
+  location: 'Jodhpur, India',
+  email: 'amardeepchauhanknp@gmail.com',
+  github: 'https://github.com/Roopshree6902',
+  twitterHandle: '@Roopshr90263033',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/amardeep123468',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/amardeepsinghchauhan/',
+    },
+    {
+      name: 'Leetcode',
+      url: 'https://leetcode.com/Shristha/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/amardeep_chauhann/',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://www.instagram.com/amardeep_chauhann/',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
